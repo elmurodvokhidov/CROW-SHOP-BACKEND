b7247f020d9b14874d916ddfcf617863767ce193
Muxitdin <toraananas@gmail.com>
2024-09-13 14:16:45 +0500

    Merged changes from nigora
    worked on user-model/controller/routes

--- start/middlewares.js
+++ start/middlewares.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 module.exports = function (app) {
     app.use(express.json());
     app.use(cors());
+    app.use(bodyParser.json());
 };
---
+++ user/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { clerkController } = require("./userController");
+const router = express.Router()
+
+router.post('/clerk-webhook', clerkController);
+
+module.exports = router
